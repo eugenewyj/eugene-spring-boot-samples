19e4630af6aa4bfeaef124420265783937c2847d
Eugene Wang <fengbaoxp@gmail.com>
2017-10-12 18:36:25 +0800

    添加java9 ImmutableList示例

---
+++ java9-sample/src/main/java/book/java9byexample/sort/App.java
@@ -0,0 +1,7 @@
+package book.java9byexample.sort;
+
+/**
+ * @since 2017/10/12
+ */
+public class App {
+}
---
+++ java9-sample/src/test/java/java9/util/ImmutableListTest.java
@@ -0,0 +1,7 @@
+package java9.util;
+
+/**
+ * @since 2017/10/12
+ */
+public class ImmutableListTest {
+}
