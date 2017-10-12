650a97b8ad0e0bbde6eede475bec7e604406c7c3
Eugene Wang <fengbaoxp@gmail.com>
2017-10-12 18:36:42 +0800

    添加java9 ImmutableList示例

--- java9-sample/src/main/java/book/java9byexample/sort/App.java
+++ java9-sample/src/main/java/book/java9byexample/sort/App.java
@@ -4,4 +4,14 @@ package book.java9byexample.sort;
  * @since 2017/10/12
  */
 public class App {
+    public static void main(String[] args) {
+        String[] actualNames = new String[]{"Johnson", "Wilson", "Wilkinson", "Abraham", "Dagobert"};
+        final Sort sorter = new Sort();
+        sorter.sort(actualNames);
+        for (final String name : actualNames) {   
+            System.out.println(name);
+        }
+        
+    }
 }
+                                 
--- java9-sample/src/test/java/book/java9byexample/sort/SimplestStringListSortTest.java
+++ java9-sample/src/test/java/book/java9byexample/sort/SimplestStringListSortTest.java
@@ -1,7 +1,20 @@
 package book.java9byexample.sort;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * @since 2017/10/12
  */
 public class SimplestStringListSortTest {
+    @Test
+    public void canSortStrings() {
+        ArrayList actualNames = new ArrayList(Arrays.asList("Johnson", "Wilson", "Wilkinson", "Abraham", "Dagobert"));
+        Collections.sort(actualNames);
+        Assertions.assertIterableEquals(new ArrayList<String>(Arrays.<String>asList("Abraham", "Dagobert", "Johnson", "Wilkinson", "Wilson")), actualNames);
+    }
 }
